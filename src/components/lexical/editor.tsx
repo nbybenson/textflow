cb7bb72868239c257cb6a4e80f0c47014dc1d9f6
nbybenson <nbybenson@gmail.com>
2025-03-24 17:17:11 +0800

    Added functionality to store editor text on a state

--- src/components/lexical/editor.tsx
+++ src/components/lexical/editor.tsx
@@ -2,45 +2,67 @@
 
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { EditorState, SerializedEditorState } from "lexical";
+import { useMemo, useState } from "react";
 
-const theme = {}
+const theme = {
+    
+}
 
 function onError(error: any) {
     console.error(error);
 }
 
+// function OnChangePlugin({ onChange }: any) {
+//     const [editor] = useLexicalComposerContext();
+//     useEffect(() => {
+//         return editor.registerUpdateListener(({ editorState }) => {
+//             onChange(editorState);
+//         });
+//     }, [editor, onChange]);
+// }
+
 function Editor() {
     const initialConfig = {
         namespace: "Editor",
-        theme,
         onError,
     }
 
-    // const placeholder = useMemo(() => {
-    //     return (
-    //         <div>
-    //             <p>Type something...</p>
-    //         </div>
-    //     )
-    // })
+    const placeholder = useMemo(() => {
+        return (
+            <div className="absolute top-2 left-2">
+                <p>Once upon a time, ...</p>
+            </div>
+        )
+    }, [])
+
+    const [editorState, setEditorState] = useState<string>();
+    const onChange = (editorState: EditorState) => {
+        const editorStateJSON = editorState.toJSON();
+        setEditorState(JSON.stringify(editorStateJSON));
+    }
 
     return (
         <LexicalComposer initialConfig={initialConfig}>
             <RichTextPlugin
                 contentEditable={
                     <ContentEditable
-                        aria-placeholder="Type something..."
-                        placeholder={<div>Type something...</div>}
+                        aria-placeholder="abc..."
+                        placeholder={placeholder}
+                        className="p-2 rounded-lg"
                     />
                 }
                 ErrorBoundary={LexicalErrorBoundary}
             />
             <HistoryPlugin />
             <AutoFocusPlugin />
+            <OnChangePlugin onChange={onChange} />
         </LexicalComposer>
     )
 }
